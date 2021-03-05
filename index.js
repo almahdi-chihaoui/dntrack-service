72c8af91f0736ec00529dd32c86b50b4791337fe
Mahdi Chihaoui <almahdichihaoui@gmail.com>
2021-03-05 17:58:16 +0100

    use strict

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+'use strict'
+
 const express = require('express');
 const { httpLogger } = require('./src/middlewares');
 const { logger } = require('./src/utils');
@@ -6,18 +8,6 @@ const PORT = 3005;
 const app = express();
 
 app.use(httpLogger);
-
-app.get('/', (req, res) => {
-  res.send('Hello World!');
-});
-
-app.get('/home', (req, res) => {
-  res.send('Hello from home endpoint!');
-});
-
-app.get('/user', (req, res) => {
-  res.send('Hello from user endpoint!');
-});
 
 app.listen(PORT, () => {
   logger.info(`Server listening on port ${PORT}`);
--- src/utils/logger.js
+++ src/utils/logger.js
@@ -1,3 +1,5 @@
+'use strict'
+
 const { createLogger, transports, format } = require('winston');
 
 const logger = createLogger({
