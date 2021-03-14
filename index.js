b09c95f2ec35e1f2c11befdfd22fd8c6a1119318
Mahdi Chihaoui <almahdichihaoui@gmail.com>
2021-03-14 15:17:38 +0100

    use routes and first test of the api

--- index.js
+++ index.js
@@ -1,15 +1,43 @@
 'use strict'
 
 const express = require('express');
+var bodyParser = require('body-parser')
+
 const { httpLogger } = require('./src/middlewares');
+const routes = require('./src/routes');
+
 const { logger } = require('./src/utils');
+
+const {
+  connectionsManager,
+  trackersManager,
+} = require('./src/tracking-manager');
 
 const PORT = 3005;
 const app = express();
 
+// Parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+
+// Parse application/json
+app.use(bodyParser.json())
+
 app.use(httpLogger);
 
-require('./src');
+app.use(routes(trackersManager, connectionsManager));
+
+// require('./src');
+
+// Error handler
+app.use(function (err, req, res, next) {
+  res.status(err.status || 500);
+  res.send({
+    type: 'error',
+    status: err.status,
+    message: err.message,
+    stack: req.app.get('env') === 'development' ? err.stack : undefined
+  });
+});
 
 app.listen(PORT, () => {
   logger.info(`Server listening on port ${PORT}`);
